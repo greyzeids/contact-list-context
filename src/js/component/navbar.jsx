7ac97cc21f8b1e0bc31f30f68d9b6c9b2565c32d
Miquel Carnot <mcgraphicss@gmail.com>
2024-06-28 21:36:06 +0200

    Changes

---
+++ src/js/component/navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const Navbar = () => {
+    return (
+        <nav className="navbar navbar-light bg-light mb-3">
+            <div className="ml-auto">
+                <Link to="/demo">
+                    <button className="btn btn-primary">
+                        Check the Context in action
+                    </button>
+                </Link>
+            </div>
+        </nav>
+    );
+};
